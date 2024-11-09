e2feb3d2e2f2c135687a63666b107f4e6df1c40e
iris050709 <ic8281932@gmail.com>
2024-11-08 20:43:36 -0600

    RUTAS

--- routes/web.php
+++ routes/web.php
@@ -48,9 +48,49 @@ Route::get('/home', function(){
 
 
 Route::middleware([CheckAdmin::class])->group(function(){
-    Route::resource('books', BookController::class);
-    Route::resource('authors', AuthorController::class);
-    Route::resource('editorials', EditorialController::class);
-    Route::resource('genres', GenreController::class);
-    Route::resource('loans', LoanController::class);
+    // Rutas para books (BookController)
+    Route::get('books', [BookController::class, 'index']);
+    Route::get('books/create', [BookController::class, 'create']);
+    Route::post('books', [BookController::class, 'store']);
+    Route::get('books/{book}', [BookController::class, 'show']);
+    Route::get('books/{book}/edit', [BookController::class, 'edit']);
+    Route::put('books/{book}', [BookController::class, 'update']);
+    Route::delete('books/{book}', [BookController::class, 'destroy']);
+
+    // Rutas para authors (AuthorController)
+    Route::get('authors', [AuthorController::class, 'index']);
+    Route::get('authors/create', [AuthorController::class, 'create']);
+    Route::post('authors', [AuthorController::class, 'store']);
+    Route::get('authors/{author}', [AuthorController::class, 'show']);
+    Route::get('authors/{author}/edit', [AuthorController::class, 'edit']);
+    Route::put('authors/{author}', [AuthorController::class, 'update']);
+    Route::delete('authors/{author}', [AuthorController::class, 'destroy']);
+
+    // Rutas para editorials (EditorialController)
+    Route::get('editorials', [EditorialController::class, 'index']);
+    Route::get('editorials/create', [EditorialController::class, 'create']);
+    Route::post('editorials', [EditorialController::class, 'store']);
+    Route::get('editorials/{editorial}', [EditorialController::class, 'show']);
+    Route::get('editorials/{editorial}/edit', [EditorialController::class, 'edit']);
+    Route::put('editorials/{editorial}', [EditorialController::class, 'update']);
+    Route::delete('editorials/{editorial}', [EditorialController::class, 'destroy']);
+
+    // Rutas para genres (GenreController)
+    Route::get('genres', [GenreController::class, 'index']);
+    Route::get('genres/create', [GenreController::class, 'create']);
+    Route::post('genres', [GenreController::class, 'store']);
+    Route::get('genres/{genre}', [GenreController::class, 'show']);
+    Route::get('genres/{genre}/edit', [GenreController::class, 'edit']);
+    Route::put('genres/{genre}', [GenreController::class, 'update']);
+    Route::delete('genres/{genre}', [GenreController::class, 'destroy']);
+
+    // Rutas para loans (LoanController)
+    Route::get('loans', [LoanController::class, 'index']);
+    Route::get('loans/create', [LoanController::class, 'create']);
+    Route::post('loans', [LoanController::class, 'store']);
+    Route::get('loans/{loan}', [LoanController::class, 'show']);
+    Route::get('loans/{loan}/edit', [LoanController::class, 'edit']);
+    Route::put('loans/{loan}', [LoanController::class, 'update']);
+    Route::delete('loans/{loan}', [LoanController::class, 'destroy']);
+
 });
